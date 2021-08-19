8a024f854529f7a81c6a4555bc7b49172e04c620
daksh100 <82807069+daksh100@users.noreply.github.com>
2021-08-19 13:49:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+var iss, spacecraft;
+var bg, issimg, scimg;
+var hasDocked = false;
+
+function preload(){
+  bg= loadImage("spacebg.jpg");
+  issimg = loadImage("iss.png");
+  scimg = loadImage("spacecraft1.png");
+  scimg1 = loadImage("spacecraft2.png");
+  scimg2= loadImage("spacecraft3.png");
+  scimg3= loadImage("spacecraft4.png");
+}
+function setup() {
+  createCanvas(1280, 690);
+  spacecraft = createSprite(685,540);
+  spacecraft.addImage(scimg);
+  spacecraft.scale = 0.3;
+  
+  iss = createSprite(650,330);
+  iss.addImage(issimg);
+  iss.scale = 0.9;
+}
+function draw() {
+  background(bg);
+  
+  spacecraft.addImage(scimg);
+  if(!hasDocked){
+    spacecraft.x = spacecraft.x + random(-1,1);
+    
+  if(keyDown("UP_ARROW")){
+    spacecraft.y = spacecraft.y -2;
+  }
+    
+  if(keyDown("LEFT_ARROW")){
+      spacecraft.addImage(scimg3);
+    spacecraft.x = spacecraft.x - 1;
+  }
+    
+  if(keyDown("RIGHT_ARROW")){
+      spacecraft.addImage(scimg2);
+    spacecraft.x = spacecraft.x + 1;
+  }
+    
+  if(keyDown("DOWN_ARROW")){
+      spacecraft.addImage(scimg1);
+  }
+}
+  if(spacecraft.y <= (iss.y+70) && spacecraft.x <= (iss.x-10)){
+    hasDocked = true;
+    textSize(25);
+    fill("white")
+    text("Docking Successful!", 600, 600);
+  }
+  drawSprites();
+}
